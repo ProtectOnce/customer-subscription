f16abf08888bdd3e456c458ff0100bd975d6a486
Rahul <rahul@Rahuls-MacBook-Pro.local>
2023-09-26 13:39:31 +0530

    🔧 chore(.env copy): update environment variable names to use REACT_APP prefix for consistency with Create React App 🔧 chore(App.js): update references to environment variables to use REACT_APP prefix for consistency with Create React App 🔧 chore(amplify-config.js): update references to environment variables to use REACT_APP prefix for consistency with Create React App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { API, graphqlOperation } from 'aws-amplify';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import './amplify-config';
 
@@ -15,12 +15,15 @@ const onCreateApplicationEvent = /* GraphQL */ `
 `;
 
 function App() {
+  const [subscriptionData, setSubscriptionData] = useState(null);
+
   useEffect(() => {
     const subscription = API.graphql(
-      graphqlOperation(onCreateApplicationEvent, { tenantId: process.env.TENANTID })
+      graphqlOperation(onCreateApplicationEvent, { tenantId: process.env.REACT_APP_TENANT_ID })
     ).subscribe({
       next: (eventData) => {
         console.log(`[SUBSCRIPTION] CreateApplicationEvent - Data received:`, JSON.stringify(eventData, null, 2));
+        setSubscriptionData(eventData.value.data.oncreateApplicationEvent);
       },
       error: (error) => {
         console.error(`[SUBSCRIPTION] CreateApplicationEvent - Error:`, error);
@@ -33,6 +36,14 @@ function App() {
   return (
     <div className="App">
       <h1>Amplify Subscriptions Example</h1>
+      {subscriptionData ? (
+        <div>
+          <h3>Subscription Data:</h3>
+          <pre>{JSON.stringify(subscriptionData, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>Waiting for subscription data...</p>
+      )}
     </div>
   );
 }
--- src/amplify-config.js
+++ src/amplify-config.js
@@ -2,8 +2,8 @@ import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
   aws_project_region: 'us-east-2',
-  aws_appsync_graphqlEndpoint: process.env.ENDPOINT,
+  aws_appsync_graphqlEndpoint: process.env.REACT_APP_ENDPOINT,
   aws_appsync_region: 'us-east-2',
   aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: process.env.APIKEY,
+  aws_appsync_apiKey: process.env.REACT_APP_APIKEY,
 });
